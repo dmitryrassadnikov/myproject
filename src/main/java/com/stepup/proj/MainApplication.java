601178ad52a918341b77a34062d55564652f1759
Dmitry Rassadnikov <dmitryrassadnikov@gmail.com>
2024-02-24 17:57:39 +0300

    Изменен метод doSomething()

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,11 +4,12 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("100001");
-
     }
 
     public static void doSomething() {
         System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
     }
 }
 
