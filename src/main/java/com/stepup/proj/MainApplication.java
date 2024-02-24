5acd98a4448b25a7b22f78b7bc4b2c1d98ba8fac
Dmitry Rassadnikov <dmitryrassadnikov@gmail.com>
2024-02-24 17:46:43 +0300

    Изменена основная логика Добавлен класс коробки

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,7 +3,12 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("111111");
+        System.out.println("100001");
+
+    }
+
+    public static void doSomething() {
+        System.out.println(1);
     }
 }
 
