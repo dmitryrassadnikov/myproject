6e090b6871052e4b6b869f1045459f49c688f140
Dmitry Rassadnikov <dmitryrassadnikov@gmail.com>
2024-04-11 04:13:10 +0300

    Добавлено решение задачи "#3.ООП. Пакеты. Возведение в степень".

--- src/main/java/ru/stepup/courses/main/Main.java
+++ src/main/java/ru/stepup/courses/main/Main.java
@@ -1,8 +1,30 @@
+/*
+#3.ООП. Пакеты. Возведение в степень.
+Создайте метод принимающий две строки, в которых будут записаны числа X и Y.
+Возвращает метод результат возведения X в степень Y. Для преобразования строки в число следует использовать
+метод Integer.parseInt, а для возведения в степень метод Math.pow. Вызовите разработанный метод передав
+туда параметры командной строки полученные точкой входа в программу. Реализуйте метод так, что бы для возведения
+в степень и преобразования строки использовались короткие имена статических методов (pow и parseInt).
+*/
+
+
 package ru.stepup.courses.main;
 
+import java.util.Scanner;
+
 public class Main {
+    public static double power(String x, String y) {
+        int num = Integer.parseInt(x);
+        int powNum = Integer.parseInt(y);
+        return Math.pow(num, powNum);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Start application!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        String x = scanner.nextLine();
+        System.out.print("Введите степень числа: ");
+        String y = scanner.nextLine();
+        System.out.println(power(x, y));
     }
 }
-
